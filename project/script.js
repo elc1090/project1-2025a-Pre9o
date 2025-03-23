227fc74dd9f43602d45841d44247349e12896b7e
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2025-03-23 11:17:15 -0300

    button return to top

--- project/script.js
+++ project/script.js
@@ -14,3 +14,23 @@ productImages.forEach((product) => {
         backImage.style.display = 'none';
     });
 });
+
+const scrollTopButton = document.querySelector('.scroll-top');
+const mainImage = document.querySelector('.imagem-principal');
+
+window.addEventListener('scroll', () => {
+    const mainImageHeight = mainImage.offsetHeight;
+
+    if (window.scrollY > mainImageHeight) {
+        scrollTopButton.classList.add('show'); 
+    } else {
+        scrollTopButton.classList.remove('show'); 
+    }
+});
+
+scrollTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
